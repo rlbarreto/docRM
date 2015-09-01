396516f5c489305153e8b6accb5ba83ab8144c65
Rafael Lessa Barreto <rafael@navesbarreto.com>
2015-08-31 21:12:03 -0300

    adicionado funcao para setar cabecalho

--- config/init.js
+++ config/init.js
@@ -10,8 +10,16 @@
     };
 
     function init(app) {
+
         app.use(logger('dev'));
+        app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
+        app.use(function setCabecalhosResponse(req, res, next) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+            next();
+        });
 
     };
 })();
